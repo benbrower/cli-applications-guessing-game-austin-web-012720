afc4dfbcded6d720a293bbb220c7cfbc21724423
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-30 02:59:44 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,15 @@
 # Code your solution here!
+def run_guessing_game
+  current_number = rand (1..6)
+  puts "Guess a number between 1 and 6"
+  user_guess = gets.chomp
+
+  if user_guess == "exit"
+    puts "Goodbye!"
+  elsif user_guess != current_number
+    puts "Sorry! The computer guessed #{current_number}"
+  elsif user_guess == current_number
+    puts "You guess the correct number!"
+  end
+end
+    
